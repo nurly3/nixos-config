43646d49d418e64521e708dcad3e18bdaf8699cd
Nurlyx <nurlyx@proton.me>
2025-08-30 12:23:21 +0200

    remove greeting with hacks

--- home-modules/fish.nix
+++ home-modules/fish.nix
@@ -1,17 +1,16 @@
 { ... }:
 
 {
-  programs.fish = {
-    enable = true;
-    
-    shellAliases = {
-      "hr" = "home-manager switch --flake .#nurlyx";
-      "nr" = "sudo nixos-rebuild switch --flake .#main --impure";
-    };
+	programs.fish = {
+		enable = true;
 
-    extraConfig = ''
-      set -g fish_greeting ""
-    '';
-  };
+		shellAliases = {
+			"hr" = "home-manager switch --flake .#nurlyx";
+			"nr" = "sudo nixos-rebuild switch --flake .#main --impure";
+		};
+
+		configFile = pkgs.writeText "config.fish" ''
+			set -g fish_greeting ""
+			'';;
+	}
 }
-
