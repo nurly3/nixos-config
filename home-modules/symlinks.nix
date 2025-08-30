0649a94a93ff32d46bac7d480e31e4c3a7ef3242
Nurlyx <nurlyx@proton.me>
2025-08-30 15:56:43 +0200

    kitty colorscheme

--- home-modules/symlinks.nix
+++ home-modules/symlinks.nix
@@ -3,7 +3,12 @@
 {
   xdg.configFile = {
   "niri/config.kdl".source = ./niri/config.kdl;
+
+  # Kitty
   "kitty/kitty.conf".source = ./kitty/kitty.conf;
+  "kitty/Catppuccin-Mocha.conf".source = ./kitty/Catppuccin-Mocha.conf;
+  
+
   "fish/config.fish".source = ./fish/config.fish;
   "starship.toml".source = ./starship/starship.toml;
   };
