d02020b6fba874e99c641e00e07da2493b195dda
Nurlyx <nurlyx@proton.me>
2025-08-30 12:21:13 +0200

    remove greeting

--- home-modules/fish.nix
+++ home-modules/fish.nix
@@ -8,6 +8,10 @@
       "hr" = "home-manager switch --flake .#nurlyx";
       "nr" = "sudo nixos-rebuild switch --flake .#main --impure";
     };
+
+    extraConfig = ''
+      set -g fish_greeting ""
+    '';
   };
 }
 
