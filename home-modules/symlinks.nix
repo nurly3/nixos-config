44c0325b141d9d3279e36be2dac75501c303e13a
Nurlyx <nurlyx@proton.me>
2025-08-30 12:53:45 +0200

    starship config symlink

--- home-modules/symlinks.nix
+++ home-modules/symlinks.nix
@@ -5,6 +5,7 @@
   "niri/config.kdl".source = ./niri/config.kdl;
   "kitty/kitty.conf".source = ./kitty/kitty.conf;
   "fish/config.fish".source = ./fish/config.fish;
+  "starship.toml".source = ./starship/starship.toml;
   };
 
   home.file = {
