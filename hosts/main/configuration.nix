ddac231b9181f8b4a4c3b81872917ae699caf255
Nurlyx <nurlyx@proton.me>
2025-08-30 16:20:10 +0200

    config changes

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -5,6 +5,7 @@
     [
       /etc/nixos/hardware-configuration.nix
       ../../modules/nixos/packages.nix
+      ../../modules/nixos/locale.nix
       inputs.niri-flake.nixosModules.niri
     ];
 
@@ -14,20 +15,6 @@
   networking.hostName = "${hostname}"; # Define your hostname.
   networking.networkmanager.enable = true;
   time.timeZone = "Europe/Berlin";
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
-
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -69,13 +56,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-git
-home-manager
-  ];
-nixpkgs.overlays = [ inputs.niri-flake.overlays.niri ];
+  nixpkgs.overlays = [ inputs.niri-flake.overlays.niri ];
   programs.niri.package = pkgs.niri-unstable;
   programs.niri.enable = true;
 
