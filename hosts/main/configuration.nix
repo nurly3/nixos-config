a1c82f17ec1239131d4672c8f1a7ac75d82bfe58
Nurlyx <nurlyx@proton.me>
2025-08-30 17:51:55 +0200

    my final thing

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -2,17 +2,50 @@
 
 {
   imports = [
-      ../../modules/nixos/default.nix
-      
+    ../../modules/nixos/default.nix
+
   ];
+
+  services.xserver.enable = true;
+
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
 
   users.users.${username} = {
     isNormalUser = true;
     description = "User";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-#  thunderbird
-    ];
+    packages = with pkgs; [ ];
+  }
+
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
   };
 
   modules.enableNiri = false;
--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -3,10 +3,9 @@
 { 
   imports = [ 
     /etc/nixos/hardware-configuration.nix 
-    ../../modules/nixos/packages.nix 
-    ../../modules/nixos/locale.nix 
-    ../../modules/nixos/x11_gdm_gnome.nix 
-    ../../modules/nixos/audio.nix 
-    ../../modules/nixos/niri.nix 
+    ./niri.nix 
+
+    # System
+    ./packages.nix
   ] 
 }
