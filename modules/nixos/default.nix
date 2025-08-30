ae400e2c32737f337ba1b2a1c69069b2a12ca030
Nurlyx <nurlyx@proton.me>
2025-08-30 17:37:18 +0200

    config changes

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,12 +1,29 @@
-{ ... }:
+{ config, lib, ... }:
 
-{
+let
+  enablePackages = lib.mkEnableOption "packages";
+  enableLocale = lib.mkEnableOption "locale";
+  enableX11GDMGnome = lib.mkEnableOption "x11_gdm_gnome";
+  enableAudio = lib.mkEnableOption "audio";
+  enableNiri = lib.mkEnableOption "niri";
+  
+in {
+  config = {
+    modules.enablePackages = enablePackages;
+    modules.enableLocale = enableLocale;
+    modules.enableX11GDMGnome = enableX11GDMGnome;
+    modules.enableAudio = enableAudio;
+    modules.enableNiri = enableNiri;
+  };
+
   imports = [
     /etc/nixos/hardware-configuration.nix
-      ../../modules/nixos/packages.nix
-      ../../modules/nixos/locale.nix
-      ../../modules/nixos/x11_gdm_gnome.nix
-      ../../modules/nixos/audio.nix
-      ../../modules/nixos/niri.nix
-  ]
+
+    (if config.modules.enablePackages then ../../modules/nixos/packages.nix else null)
+    (if config.modules.enableLocale then ../../modules/nixos/locale.nix else null)
+    (if config.modules.enableX11GDMGnome then ../../modules/nixos/x11_gdm_gnome.nix else null)
+    (if config.modules.enableAudio then ../../modules/nixos/audio.nix else null)
+    (if config.modules.enableNiri then ../../modules/nixos/niri.nix else null)
+  ];
 }
+
