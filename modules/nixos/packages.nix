1dd3ae0e2a0b9621ab592c26972e80ea0e730cd4
Nurlyx <nurlyx@proton.me>
2025-08-30 16:18:48 +0200

    refactoring

--- modules/nixos/packages.nix
+++ modules/nixos/packages.nix
@@ -1,8 +1,8 @@
-{ ... }: 
+{ pkgs, ... }: 
 
 {
-	environment.systemPackages = with pkgs; [
-		git
-		home-manager
-	];
+  environment.systemPackages = with pkgs; [
+    git
+      home-manager
+  ];
 }
