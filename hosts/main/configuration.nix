6ff278a7eafe4217ce7e65944757a122cb5e4ef1
Nurlyx <nurlyx@proton.me>
2025-08-30 15:15:39 +0000

    config fix

--- hosts/main/configuration.nix
+++ hosts/main/configuration.nix
@@ -5,7 +5,6 @@
     /etc/nixos/hardware-configuration.nix
       ../../modules/nixos/packages.nix
       ../../modules/nixos/locale.nix
-      ../../modules/nixos/networking_hostname.nix
       ../../modules/nixos/x11_gdm_gnome.nix
       ../../modules/nixos/audio.nix
       ../../modules/nixos/niri.nix
