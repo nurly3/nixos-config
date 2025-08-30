8a7fd15301fe4323ad92e714bb52f124046083da
Nurlyx <nurlyx@proton.me>
2025-08-30 11:03:12 +0200

    I probably messed up

--- home-modules/niri/niri.nix
+++ home-modules/niri/niri.nix
@@ -1,8 +1,8 @@
-{ ... }:
+{ pkgs, ... }: 
 
 {
-	programs.niri = {
-		enable = true;
-		config = null;
-	};
+  nixpkgs.overlays = [ inputs.niri-flake.overlays.niri ];
+  programs.niri.package = pkgs.niri-unstable;
+  programs.niri.config = null;
 }
+
