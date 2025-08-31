118c0ed41f8a7906e8402a85cffc1d95d36dccf9
Nurlyx <nurlyx@proton.me>
2025-08-31 13:54:15 +0200

    I just want to use my pc

--- home-manager/fish/config.fish
+++ home-manager/fish/config.fish
@@ -1,4 +1,4 @@
 set -g fish_greeting ''
-alias hr "home-manager switch --flake .#$USER"
+alias hr "home-manager switch --flake .#$USER --impure"
 alias nr "sudo nixos-rebuild switch --flake .#main --impure"
 starship init fish | source
