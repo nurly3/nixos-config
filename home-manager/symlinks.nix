8346b584e3e289ad5caf4a255c7d2bcd4b1d8021
Nurlyx <nurlyx@proton.me>
2025-08-31 17:13:35 +0200

    variable

--- home-manager/symlinks.nix
+++ home-manager/symlinks.nix
@@ -1,18 +1,16 @@
 { config, lib, ... }:
 
-let
-liveLink = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/
-in
 {
   xdg.configFile = {
-  "niri/config.kdl".source = ${liveLink}/niri/config.kdl;
-  "kitty/kitty.conf".source = ${liveLink}/kitty/kitty.conf;
-  "fish/config.fish".source = ${livelink}/fish/config.fish;
-  "starship.toml".source = ${liveLink}/starship/starship.toml;
-  "nvim/".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/nvim;
+    "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/niri/config.kdl;
+    "kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/kitty/kitty.conf;
+    "fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/fish/config.fish;
+    "starship.toml".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/starship/starship.toml;
+    "nvim/".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/nvim;
+    "matugen/".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/matugen/;
   };
 
   home.file = {
-  "Pictures/wallpaper.jpg".source = ./niri/wallpaper.jpg;
+    "Pictures/wallpaper.jpg".source = ./niri/wallpaper.jpg;
   };
 }
