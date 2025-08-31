7051afc64a2e52d2a65089de9d9634f859b4d472
Nurlyx <nurlyx@proton.me>
2025-08-31 13:51:54 +0200

    don't blame me

--- home-manager/symlinks.nix
+++ home-manager/symlinks.nix
@@ -1,11 +1,11 @@
-{ config, lib, username, ... }:
+{ config, lib, ... }:
 
 {
   xdg.configFile = {
-  "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink /home/${username}/nixos-config/home-manager/niri/config.kdl;
-  "kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink /home/${username}/nixos-config/home-manager/kitty/kitty.conf;
-  "fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink /home/${username}/nixos-config/home-manager/fish/config.fish;
-  "starship.toml".source = config.lib.file.mkOutOfStoreSymlink /home/${username}/nixos-config/home-manager/starship/starship.toml;
+  "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/niri/config.kdl;
+  "kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/kitty/kitty.conf;
+  "fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/fish/config.fish;
+  "starship.toml".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/starship/starship.toml;
   };
 
   home.file = {
