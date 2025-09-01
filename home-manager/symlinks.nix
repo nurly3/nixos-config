56cc70b46fd726ff62dc5d6577527558ebfb42c6
Nurlyx <nurlyx@proton.me>
2025-09-01 10:23:14 +0200

    No starship

--- home-manager/symlinks.nix
+++ home-manager/symlinks.nix
@@ -5,7 +5,6 @@
     "niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/niri/config.kdl;
     "kitty/kitty.conf".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/kitty/kitty.conf;
     "fish/config.fish".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/fish/config.fish;
-    "starship.toml".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/starship/starship.toml;
     "nvim/".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/nvim;
     "matugen/".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/matugen;
     "fuzzel/fuzzel.ini".source = config.lib.file.mkOutOfStoreSymlink ~/nixos-config/home-manager/fuzzel/fuzzel.ini;
